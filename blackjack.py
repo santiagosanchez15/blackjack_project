39319bd0315a69695e02b3b1f357749627b4da9b
Santiago Sánchez <santiagosanchez@Santiagos-MacBook-Pro-7.local>
2025-10-10 22:34:11 -0700

    function great

--- blackjack.py
+++ blackjack.py
@@ -1,7 +1,22 @@
 class Player:
-    def __init__(self, wallet = 20, name = "player"):
+    '''Defaul player class'''
+    def __init__(self, name = "player", wallet = 20, wins = 0, loses = 0):
         self.name = name
         self.wallet = wallet
+        self.wins = wins
+        self.loses = loses
+        
 
     def __repr__(self):
-        return f"This is {self.name} and this player has ${self.wallet} in his wallet"
+        return f"This is {self.name} and this player has ${self.wallet} in his wallet.\nThe amount of times {self.name} has won are {self.wins} and the amount of times that {self.name} has lost are {self.loses} times"
+
+def great():
+    ''''Great and get name'''
+    print("Hello, thank you for coming to Casino Royale")
+    print("Before playing, could I check your ID")
+    player1 = Player()
+    player1.name = input("Oh, see your name is: ")
+    return f"Perfect {player1.name}, Welcome, the BlackJack table is right this way..."
+
+print(great())
+
