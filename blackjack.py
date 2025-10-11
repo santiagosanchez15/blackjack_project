08be9f6e0a0b4959d41c3d3cba46bba81305ad44
Santiago Sánchez <santiagosanchez@Santiagos-MacBook-Pro-7.local>
2025-10-11 10:46:50 -0700

    House class

--- blackjack.py
+++ blackjack.py
@@ -3,7 +3,7 @@ import random
 class Player:
     '''Defaul player class'''
     def __init__(self, name = "player", wallet = 20, wins = 0, loses = 0, cards = None):
-        # Define players attributes
+        ''' Define players attributes'''
         self.name = name
         self.wallet = wallet
         self.wins = wins
@@ -13,6 +13,19 @@ class Player:
 
     def __repr__(self):
         return f"This is {self.name} and this player has ${self.wallet} in his wallet.\nThe amount of times {self.name} has won are {self.wins} and the amount of times that {self.name} has lost are {self.loses} times"
+
+class House:
+    '''Class House menaing casino'''
+    def __init__(self, name = "The House", wins = 0, loses = 0, cards = None):
+        '''Define House attributes'''
+        self.name = name
+        self.wins = wins
+        self.loses = loses
+        self.cards = cards if cards is not None else []
+
+    def __repr__(self):
+        '''House description'''
+        return f"This is {self.name}, the casino itself, it has {self.wins} wins and {self.loses} loses, be careful"
 
 def great():
     ''''Great and get name'''
@@ -61,10 +74,13 @@ def blackjack(player):
     print(f"Oh you got a {player_first_card} and a {player_second_card}")
     player = Player
     player.cards = [player_first_card, player_second_card]
-
     return player_count(player)
     
+def house():
+    list_cards = ["A", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+    house_first_card = draw_cards(list_cards)
+    house_second_card = draw_cards(list_cards)
+    print(f"Oh you got a {house_first_card} and a {house_second_card}")
 
-juan = Player
-print(blackjack(juan))
+
 
