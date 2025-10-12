cb76d319afa5bbf3f9fb78d4c7ac6104bef547bb
Santiago Sánchez <santiagosanchez@Santiagos-MacBook-Pro-7.local>
2025-10-12 10:07:28 -0700

    Function house and house ace

--- blackjack.py
+++ blackjack.py
@@ -49,6 +49,26 @@ def Ace_checker():
         A_input = input("Do you want your Ace to be 1 or 11: ")
     return A_input
 
+def Ace_house_checker(total_value):
+    if total_value <= 10:
+        return 11
+    else:
+        return 1        
+
+def house_count(house):
+    '''Count total cards of player'''
+    # Create accumulator, set value of cards
+    total_value = 0
+    deck_cards = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, "J": 10, "Q": 10, "K": 10, 11: 11}
+    for card in house.cards:
+        if card == "A":
+                card = Ace_house_checker(total_value)
+                total_value += deck_cards[card]
+        elif card in deck_cards:
+            total_value += deck_cards[card]
+    return total_value
+
+
 def player_count(player):
     '''Count total cards of player'''
     # Create accumulator, set value of cards
@@ -72,15 +92,21 @@ def blackjack(player):
     player_first_card = draw_cards(list_cards)
     player_second_card = draw_cards(list_cards)
     print(f"Oh you got a {player_first_card} and a {player_second_card}")
-    player = Player
     player.cards = [player_first_card, player_second_card]
     return player_count(player)
     
-def house():
+def house_dealer(house):
+    '''Retyrn the house cards'''
+    # Set basic actions
     list_cards = ["A", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
     house_first_card = draw_cards(list_cards)
     house_second_card = draw_cards(list_cards)
-    print(f"Oh you got a {house_first_card} and a {house_second_card}")
+    print(f"Oh the house got a {house_first_card} and a {house_second_card}")
+    house.cards = [house_first_card, house_second_card]
 
+    return house_count(house)
+    
 
+casino_royale = House
+print(house_dealer(casino_royale))
 
